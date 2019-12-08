81ca7f08df6bd2119c1dc280dc9a4c5e0696104a
jorgexyx <bautisjo@sonoma.edu>
2019-12-07 16:38:38 -0800

    fixed typo

--- ModelBuilder/ModelBuilder.py
+++ ModelBuilder/ModelBuilder.py
@@ -1,4 +1,4 @@
-from ModeBuilder.model_builder_tools import create_model
+from ModelBuilder.model_builder_tools import create_model
 class GenderDetectionModel:
     def __init__(self):
         self.model = create_model(255)
