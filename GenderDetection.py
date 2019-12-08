48dd6f88e55f78faf31d14c48a79594b91595bb5
jorgexyx <bautisjo@sonoma.edu>
2019-12-07 16:08:45 -0800

    Gender main file

---
+++ GenderDetection.py
@@ -0,0 +1,11 @@
+"""
+A CNN Model that can detect Gender:
+
+Dataset info:
+    - Dataset can be found at https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_crop.tar
+
+    - Can be loaded into google collab using 
+        !wget --no-check-certificate https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_crop.tar -O /tmp/faces.zip
+""" 
+from ModelBuilder import GenderDetectioModel
+gender_model = GenderDetectionModel()
